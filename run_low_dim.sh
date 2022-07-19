346e1afcd96433b0d9c36c0ae0bb274a06f70ca4
Roshni Sahoo <roshni714@github.com>
2022-07-19 15:16:23 -0700

    run_low

--- run_low_dim.sh
+++ run_low_dim.sh
@@ -5,8 +5,8 @@ MAX_ITER=100
 ### Toy Example Experiment
 for SEED in "${seeds[@]}";
 do
-    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save low_dim_7-12-22 --seed $SEED --gradient_type total_deriv
-    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save low_dim_7-12-22 --seed $SEED --gradient_type partial_deriv_loss_theta
+    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save low_dim_7-19-22 --seed $SEED --gradient_type total_deriv
+    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 0.25 --max_iter $MAX_ITER --n_types 10 --save low_dim_7-19-22 --seed $SEED --gradient_type partial_deriv_loss_theta
 done
 
 
