ff9e5f9f82fe33795df91c58735a8f9907c12dad
Roshni Sahoo <roshni714@github.com>
2022-07-19 15:18:42 -0700

    run high

--- run_high_dim.sh
+++ run_high_dim.sh
@@ -5,8 +5,8 @@ MAX_ITER=100
 ### High Dimensional Experiment
 for SEED in "${seeds[@]}";
 do
-    python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save sphere_7-9-22 --seed $SEED --gradient_type "total_deriv"
-    python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save sphere_7-9-22 --seed $SEED --gradient_type "partial_deriv_loss_beta"
+    python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save high_dim_7-19-22 --seed $SEED --gradient_type "total_deriv"
+    python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save high_dim_7-19-22 --seed $SEED --gradient_type "partial_deriv_loss_beta"
 done
 
 
