01fbb5db8c1c9828b88f9ceee62f91aee02cbef9
Roshni Sahoo <roshni714@github.com>
2022-08-23 16:06:35 -0700

    added files for generating agent distribution from NELS

--- data_gen.py
+++ data_gen.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from utils_nels import impute_values
+from scipy.stats import norm
+from agent_distribution import AgentDistribution
+import random
 
 def generate_covariates():
     stmeg_variables = ["STU_ID",
@@ -86,4 +89,48 @@ def generate_covariates():
             stmeg[variable] = -stmeg[variable]
     X = stmeg[stmeg_variables[2:]].to_numpy()
     socio_econ = stmeg[stmeg_variables[1:2]].to_numpy()
-    return X, socio_econ
+    stu_id = stmeg[stmeg_variables[:1]].to_numpy()
+    return X, socio_econ, stu_id
+
+def compute_gammas(socio_econ):
+    return 1/(socio_econ + 1e-1)
+
+def compute_etas(X, gammas, sigma, beta, s):
+    etas = X + (1/(2 * gammas)) * norm.pdf(s - np.dot(X, beta), 0, scale=sigma).reshape(X.shape[0], 1) * beta
+    return etas
+
+
+def generate_losses():
+    stmeg_variables = ["STU_ID", "F3ATTEND"]
+    stmeg = pd.read_csv("data/nels_88_94_stmeg3_v1_0.csv", usecols=stmeg_variables)
+    stmeg = stmeg.replace(r'^\s*$', np.nan, regex=True)
+    stmeg = stmeg.astype({"F3ATTEND": 'float32'})
+    
+    stmeg["F3ATTEND"].replace(to_replace= -9., value=0., inplace=True)
+    stmeg["F3ATTEND"].replace(to_replace= -6., value=np.nan, inplace=True)
+    stmeg["F3ATTEND"].replace(to_replace=np.nan, value=stmeg["F3ATTEND"].mean(), inplace=True)
+    
+    stmeg["F3ATTEND"] = -stmeg["F3ATTEND"]/27
+    loss_admitted = stmeg["F3ATTEND"].to_numpy()
+    stu_id = stmeg["STU_ID"].to_numpy()
+    
+    return loss_admitted, stu_id
+
+def get_agent_distribution_nels(sigma, prev_beta, prev_s, n=1000000, subsample=None, seed=0):
+    
+    np.random.seed(seed)
+    
+    X, socio_econ, stu_id = generate_covariates()
+    gammas = compute_gammas(socio_econ)
+    etas = compute_etas(X, gammas, sigma, prev_beta, prev_s)
+    etas = etas.reshape(etas.shape[0], etas.shape[1], 1)
+    gammas = gammas.flatten() * np.ones(etas.shape)
+    
+    if subsample is not None:
+        indices = np.random.choice(list(range(len(gammas))), subsample, replace=False)
+        agent_dist = AgentDistribution(n=n, d=10, n_types=subsample, types={"etas":etas[indices], "gammas":gammas[indices]}, prop=None)
+    else:
+        agent_dist = AgentDistribution(n=n, d=10, n_types=len(gammas), types={"etas":etas, "gammas":gammas}, prop=None)
+        indices = list(range(len(gammas)))
+    
+    return agent_dist, indices
