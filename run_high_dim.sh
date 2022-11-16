1b9c747f89a3ab52e84b42df6a3cb86658e706e0
Roshni Sahoo <roshni714@github.com>
2022-11-16 13:59:46 -0800

    add more scripts

--- run_high_dim.sh
+++ run_high_dim.sh
@@ -5,8 +5,9 @@ MAX_ITER=100
 ### High Dimensional Experiment
 for SEED in "${seeds[@]}";
 do
-    krenew -t -- python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 0.1 --max_iter $MAX_ITER --n_types 10 --save high_dim_11-11-22 --seed $SEED --gradient_type "total_deriv"
-    krenew -t -- python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 0.1 --max_iter $MAX_ITER --n_types 10 --save high_dim_11-11-22 --seed $SEED --gradient_type "partial_deriv_loss_beta"
+    krenew -t -- python train_beta.py main --n 1000000 --d 10 --learning_rate 0.5 --max_iter $MAX_ITER --n_types 10 --save high_dim_11-12-22 --seed $SEED --gradient_type "expected_gradient_beta_naive"
+    krenew -t -- python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 0.5 --max_iter $MAX_ITER --n_types 10 --save high_dim_11-12-22 --seed $SEED --gradient_type "total_deriv"
+    krenew -t -- python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 0.5 --max_iter $MAX_ITER --n_types 10 --save high_dim_11-12-22 --seed $SEED --gradient_type "partial_deriv_loss_beta"
 done
 
 
--- run_low_dim.sh
+++ run_low_dim.sh
@@ -5,9 +5,9 @@ MAX_ITER=100
 ### Toy Example Experiment
 for SEED in "${seeds[@]}";
 do
-    krenew -t -- python train.py main --n 1000000 --learning_rate 0.5 --perturbation_theta 0.025 --max_iter $MAX_ITER --n_types 10 --save low_dim_11-15-22 --seed $SEED --gradient_type naive_expected_total_deriv
-    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 0.5 --max_iter $MAX_ITER --n_types 10 --save low_dim_11-15-22 --seed $SEED --gradient_type total_deriv
-    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 0.5 --max_iter $MAX_ITER --n_types 10 --save low_dim_11-15-22 --seed $SEED --gradient_type partial_deriv_loss_theta
+    krenew -t -- python train.py main --n 1000000 --learning_rate 0.2 --perturbation_theta 0.025 --max_iter $MAX_ITER --n_types 10 --save low_dim_11-15-22 --seed $SEED --gradient_type naive_expected_total_deriv
+    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 0.2 --max_iter $MAX_ITER --n_types 10 --save low_dim_11-15-22 --seed $SEED --gradient_type total_deriv
+    krenew -t -- python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 0.2 --max_iter $MAX_ITER --n_types 10 --save low_dim_11-15-22 --seed $SEED --gradient_type partial_deriv_loss_theta
 done
 
 
